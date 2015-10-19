c29a069e24cd7cab6fe55ef08d928efa4845c52b
Tim Hyson <tjhyson@gmail.com>
2015-10-19 09:11:06 +0100

    user can post image

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
-
+gem 'haml'
+gem "paperclip", "~> 4.3"
+gem 'simple_form'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  resources :posts
+  root 'posts#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
